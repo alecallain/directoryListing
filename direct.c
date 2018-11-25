690777779ac6e306df50edf4697eb204766bff7d
Alec Allain <allainal@eos22.cis.gvsu.edu>
2018-11-25 02:33:38 -0500

    Added variation of code

---
+++ direct.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+
+/**
+* This class implements a select subset of
+* the ls command
+*
+* @author Allison Bolen
+* @author Alec Allain
+*/
+
+/** Instanciate methods */
+
+
+/** Global variables */
+
+
+/** Main method */
+int main (int argc, char** argv) {
+	
+	struct stat statBuf;
+	struct dirent *entryPtr;
+	DIR *dirPtr;
+	int status;
+	//int option;
+	
+	if (argv == NULL) {
+		fprintf(stderr, "\nNo specific flags were given by the user");
+		exit(1);
+	}
+	
+	if (stat(argv[2], &statBuf) < 0) {
+		fprintf(stderr, "Cannot access file or directory");
+		exit(1);
+	}
+	else {
+		dirPtr = opendir(".");
+		// gives user and groupID for each file/directory
+		if (argv[1] == "-n") {
+			while (entry = readdir(dirPtr)) {
+				stat(entryPtr->d_name, &statBuf);
+				printf("File named %-20s user number is %d and groupID is %d\n", entryPtr->d_name, statBuf.st_uid, statBuf.st_gid);
+		}
+		// gives the user the inode for each file/directory
+		else if (argv[1] == "-i") {
+			//dirPtr = opendir(".");
+			while (entryPtr = readdir(dirPtr)) {
+				stat(entryPtr->d_name, &statBuf);
+				printf("File named %-20s's iNode number is %d\n", entryPtr->d_name, statBuf.st_ino);
+			}
+		}
+	}
+
+
+}
