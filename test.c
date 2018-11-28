c531335b4f66c1c43e4ec2967c4c24656358525f
Allison Bolen <bolena@eos24.cis.gvsu.edu>
2018-11-28 17:46:31 -0500

    list files in any dir

---
+++ test.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+   DIR *dirPtr;
+   struct dirent *entryPtr;
+   struct stat statBuf;
+   // path exists check		
+   if (argc < 2) { 
+      printf ("Usage: filename required\n"); 
+      exit(1); 
+   }
+
+   if (stat (argv[1], &statBuf) < 0) { 
+      perror ("huh?  there is "); 
+      exit(1); 
+   }
+   
+ 
+ 
+ 
+   dirPtr = opendir (argv[1]);
+
+   
+   
+   
+   while ((entryPtr = readdir (dirPtr))){
+  	stat (entryPtr->d_name, &statBuf);
+  	printf("%-20s	%ld\n", entryPtr->d_name, statBuf.st_size);
+   }
+   closedir (dirPtr);
+   return 0;
+}
+
